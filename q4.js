3e8668d46786383ef25b85b71610b0d11bc38b59
Oneka Austin <neferonyx@gmail.com>
2022-09-14 15:27:21 +0100

    q4.js

---
+++ q4.js
@@ -0,0 +1,17 @@
+// The result would be 18 as let num + let add =13 num-1 = 12 add (3) and add (3)
+
+function myFunction(num){
+    console.log(add, num)
+
+    return num-1   
+}
+let num = 10
+let add = 3
+add = myFunction(add)
+add = myFunction(add)
+
+console.log (add, num)
+
+//Overall ans = add 1  num=10
+// add = 3 when the function is run it becomes 2 because of num-1
+// when the function is run again it now becomes 1 of num-1
