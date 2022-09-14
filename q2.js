7333d4ef123afca6da4a05ea48797be8a71935bd
Oneka Austin <neferonyx@gmail.com>
2022-09-14 14:42:35 +0100

    q2.js

---
+++ q2.js
@@ -0,0 +1,8 @@
+// The results would be invalid because there is no "return" with in the code block
+function myFunction(num1,num2){
+    num1+num2
+}
+const result = myFunction(5,5)
+console.log(result)
+
+// Answer came up as "undefined" rather than invalid
