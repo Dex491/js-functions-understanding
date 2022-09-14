97185387eea092184bdde64a360bf58ac6d9b238
Oneka Austin <neferonyx@gmail.com>
2022-09-14 16:45:56 +0100

    q10 & q11

---
+++ q10.js
@@ -0,0 +1,12 @@
+// The argument will be num3 because Javascript will recognise this argument.
+function myFunction(num1, num2, num3) {
+  console.log(num3, num1)
+
+}
+const num1 = 10
+const num2 = 1
+const num3 = 20
+
+myFunction(num3, num1, 100)
+
+// My answer was incorrect, Assuming the function recognises 100 due the position in the arguments and possibly recognises num3 as num1
