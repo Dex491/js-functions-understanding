a3731d363264b3daaebafd69dd5de1af15cda153
Oneka Austin <neferonyx@gmail.com>
2022-09-14 17:32:23 +0100

    q18 &q19

---
+++ q19.js
@@ -0,0 +1,7 @@
+// The value is 10 as "return" is num2 which translates to 10 in const result.
+function myFunction(num1, num2, num3) {
+  return num2
+}
+  
+  const result = myFunction(5, 10, 15)
+  console.log(result)
