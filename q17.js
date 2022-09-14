49885fc2ea5b73f74281e2b82e5de851f49e874a
Dave <Daverevell491@gmail.com>
2022-09-14 17:22:58 +0100

    q16 and 17

---
+++ q17.js
@@ -0,0 +1,9 @@
+// The value of result would be -100 as line 298 would replace num1 with -100
+function myFunction(num1) {
+    return -100
+  
+    return num1 * 10
+  }
+  
+  const result = myFunction(5)
+  console.log(result);
